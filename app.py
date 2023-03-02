a400528ded61eda397523b7af0c3d5d041d4ef36
chandresh bhandari <126777959+CHANDRESHBHANDARI@users.noreply.github.com>
2023-03-02 14:36:14 +0000

    DYNAMIC DATA FROM PYTHON

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Scientist',
+    'location': 'Delhi India',
+    'salary':'Rs. 1300000'
+  },
+  {
+    'id':2,
+    'title':'Frontend Engg.',
+    'location':'Bengluru India',
+    'salary':'Rs. 1300000'
+  },
+  {
+    'id':3,
+    'title':'Frontend Engg.',
+    'location':'USA',
+    'salary':'$ 13000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
